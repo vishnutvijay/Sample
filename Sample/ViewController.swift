afa2d0b8468759902b59e6e3ef2ecb1af282f28d
Adattiri, Ramya (Cognizant) <755435@aim00101.local>
2018-12-27 14:55:39 +0530

    Initial Commit

---
+++ Sample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Sample
+//
+//  Created by Adattiri, Ramya (Cognizant) on 27/12/18.
+//  Copyright © 2018 Adattiri, Ramya (Cognizant). All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
